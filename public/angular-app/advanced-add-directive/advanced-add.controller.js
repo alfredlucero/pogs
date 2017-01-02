cd1142a816dac58ae093a5828afcceeeb254086b
Alfred Lucero <alfred.lucero@ucla.edu>
2017-01-02 00:46:23 -0800

    making more changes to add advanced directive - all these toggle buttons woah

--- public/angular-app/advanced-add-directive/advanced-add.controller.js
+++ public/angular-app/advanced-add-directive/advanced-add.controller.js
@@ -2,6 +2,23 @@ angular.module('pogsApp').controller('AdvancedAddController', AdvancedAddControl
 
 function AdvancedAddController($http, $window, notesDataFactory) {
 	var vm = this;
-	
+	vm.advancedAddData = {
+		date : "",
+		title : "",
+		information : "",
+		tags : "",
+		location : "",
+		approxTime : "",
+		author: "",
+		icons : []
+	};
+
+	vm.setAuthor = function(author) {
+		vm.advancedAddData.author = author;
+	};
+
+	vm.isAuthorSet = function(author) {
+		return vm.advancedAddData.author === author;
+	};
 
 }
