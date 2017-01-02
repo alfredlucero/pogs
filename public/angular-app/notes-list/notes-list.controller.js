84c69efae3c99da67acacc9e3e6eb67698da994a
Alfred Lucero <alfred.lucero@ucla.edu>
2017-01-02 14:40:37 -0800

    filters for note type work now with toggled buttons

--- public/angular-app/notes-list/notes-list.controller.js
+++ public/angular-app/notes-list/notes-list.controller.js
@@ -4,6 +4,7 @@ angular.module('pogsApp').controller('NotesController', NotesController);
 function NotesController(notesDataFactory) {
 	var vm = this;
 	vm.quickFilter = '';
+	vm.noteType = 'all';
 
 	notesDataFactory.notesList().then(function(response) {
 		console.log(response);
@@ -14,15 +15,19 @@ function NotesController(notesDataFactory) {
 		return note.author === 'Pedong' || note.author === 'Pogs';
 	};
 
-	vm.pedongFilter = function(note) {
-		return note.author === 'Pedong';
-	};
-
 	vm.ginginPogsFilter = function(note) {
 		return note.author === 'Gingin' || note.author === 'Pogs';
 	};
 
-	vm.ginginFilter = function(note) {
-		return note.author === 'Gingin';
+	vm.pogsFilter = function(note) {
+		return note.author === 'Pogs';
+	};
+
+	vm.setNoteType = function(noteType) {
+		vm.noteType = noteType;
+	};
+
+	vm.isNoteTypeSet = function(noteType) {
+		return vm.noteType === noteType;
 	};
 }
